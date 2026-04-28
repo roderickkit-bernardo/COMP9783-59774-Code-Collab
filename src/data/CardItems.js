84e5fd7d2fa07cf442ba9a2d454ea49d2e29c76c
Roderick Bernardo <roderickkit.bernardo@gmail.com>
2026-04-28 10:13:18 -0400

    Update CardItems.js

--- src/data/CardItems.js
+++ src/data/CardItems.js
@@ -4,6 +4,6 @@ export const CardItems = [
     fullName: "Roderick Bernardo",
     gitHubUserName: "@roderickkit-bernardo",
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed erat libero. Morbi lectus magna, blandit id dictum vel, pellentesque nec neque.",
+      "Confucius - I hear and I forget, I see and I remember, I do and I understand.",
   },
 ];
