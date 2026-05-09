4198d3beb9068745d073acf80db7649249166f4b
Priyanka Yadav <priyankanyadav1144@gmail.com>
2026-05-09 17:50:55 -0400

    Added new CardItem entry
    Added my profile without overwriting existing data.

--- src/data/CardItems.js
+++ src/data/CardItems.js
@@ -1,5 +1,13 @@
 export const CardItems = [
   {
+    gitHubAvatar: "https://github.com/roderickkit-bernardo.png",
+    fullName: "Roderick Bernardo",
+    gitHubUserName: "@roderickkit-bernardo",
+    quote:
+      "Confucius - I hear and I forget, I see and I remember, I do and I understand.",
+  },
+
+  {
     gitHubAvatar: "https://github.com/priyankanyadav.png",
     fullName: "Priyanka Yadav",
     gitHubUserName: "@priyankanyadav",
